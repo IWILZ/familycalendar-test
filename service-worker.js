5ff0b1a99e6b2289786c95703e7fb9360311b66a
IWILZ <94467184+IWILZ@users.noreply.github.com>
2026-07-16 18:59:15 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "familycalendar-test-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./jsQR.js", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
